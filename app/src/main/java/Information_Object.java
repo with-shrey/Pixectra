d123aae4b69c2ba845d860d2f47dd034f0a7d964
swaini negi <swaininegi@gmail.com>
2018-02-19 22:30:50 +0530

    cart ui

---
+++ app/src/main/java/Information_Object.java
@@ -0,0 +1,23 @@
+/**
+ * Created by swaini negi on 19/02/2018.
+ */
+
+public class Information_Object {
+    private String post, time;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+}
