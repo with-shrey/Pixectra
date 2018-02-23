022ad56cd619659083154f2e6f1d99244845078e
cd healthyTOD <sanath.bharadwaj21@gmail.com>
2018-02-23 14:43:26 +0530

    First commit

---
+++ app/src/main/java/com/pixectra/app/PicasaActivity.java
@@ -0,0 +1,226 @@
+package com.pixectra.app;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PicasaActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1;
+    private static final int RC_AUTHORIZE_PICASA = 2;
+    private GoogleSignInClient mGoogleSignInClient;
+    static final String TAG = "GoogleSignIn";
+    FirebaseAuth mAuth;
+    RequestQueue queue;
+    private String accessToken;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picasa);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        queue = Volley.newRequestQueue(this);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSignIn();
+            }
+        });
+
+
+    }
+
+    public void onSignIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+                showToast("Getting Token");
+                getToken(data);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+        if (requestCode == RC_AUTHORIZE_PICASA) {
+           getToken(data);
+        }
+    }
+
+    void getToken(Intent data)
+    {
+        GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+        if (result.isSuccess()) {
+            final GoogleSignInAccount account = result.getSignInAccount();
+
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        String scope = "oauth2:https://picasaweb.google.com/data/";
+                        accessToken = GoogleAuthUtil.getToken(getApplicationContext(), account.getAccount(), scope, new Bundle());
+                        Log.d(TAG, "accessToken: " + accessToken);//accessToken:ya29.Gl...
+                        //getData();
+
+                    } catch (IOException | GoogleAuthException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            AsyncTask.execute(runnable);
+
+        } else {
+            showToast("result failed");
+        }
+    }
+
+
+
+    void showToast(String message)
+    {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            showToast("Success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void onFetchData(View view)
+    {
+        if(checkForPicasaAuthPermission())
+            getData();
+    }
+
+    void getData()
+    {
+        final TextView mTextView = findViewById(R.id.string_response);
+        String url = "https://picasaweb.google.com/data/feed/api/user/default?kind=photo&max-results=100?alt=json";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url,null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        mTextView.setText("Response is---     "+ response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                showToast(error.getMessage());
+                mTextView.setText("That didn't work!");
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params ;//=  super.getHeaders();
+                //if(params==null)
+                params = new HashMap<>();
+                params.put("Authorization","Bearer " + accessToken);
+                params.put("GData-Version", "3");
+                return params;
+            }
+        };
+
+        queue.add(jsonObjectRequest);
+    }
+
+    private boolean checkForPicasaAuthPermission() {
+        Scope SCOPE_PICASA = new Scope("https://picasaweb.google.com/data/");
+        String scope = "oauth2:https://picasaweb.google.com/data/";
+        if (!GoogleSignIn.hasPermissions(
+                GoogleSignIn.getLastSignedInAccount(this),
+                SCOPE_PICASA)) {
+            GoogleSignIn.requestPermissions(
+                    PicasaActivity.this,
+                    RC_AUTHORIZE_PICASA,
+                    GoogleSignIn.getLastSignedInAccount(this),
+                    SCOPE_PICASA);
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+}
